484f9384c1d97ee6bb667ac4b109bcb4acabb435
Adarsh Sharma <153820907+Adarsh-5harma@users.noreply.github.com>
2025-05-30 22:48:43 +0545

    Add files via upload

---
+++ movielist/script.js
@@ -0,0 +1,9 @@
+// GetElementById
+
+var headtitle = document.getElementById("list-heading");
+console.log(headtitle) // Output:
+headtitle.style.color = "brown"
+
+
+// getElementByClassName
+var listitem = document.getElementsByClassName("list-items")
